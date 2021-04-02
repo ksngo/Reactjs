4c096f35f5fab557f100c7750561c022aca9c90a
nks <sevenelectrons@gmail.com>
2021-04-02 11:50:50 +0800

    14

--- 14/src/modules/index.js
+++ 14/src/modules/index.js
@@ -1,4 +1,4 @@
-const { combinedReducers, combineReducers} = require('redux')
+const { combineReducers} = require('redux')
 const { reducer: movies} = require('./movies')
 
 module.exports=combineReducers({
